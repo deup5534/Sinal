c563b373b116516c83726db37d3f194ab67ed9c6
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-09-30 22:41:34 -0300

    legal

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Descubra se é... ou não é!");
+Console.ResetColor();
+
+int n1;
+
+Console.Write("Digite um número:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 <0)
+{
+    Console.WriteLine($"\n{n1} é negativo");
+}
+else
+{
+    Console.WriteLine($"\n{n1} é positivo");
+}
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("\nValeu por usar!");
+Console.ResetColor();
